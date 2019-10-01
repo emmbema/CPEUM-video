47bcf7d935d7667ff9deecf6a60e07500ad175f3
emmbema <itsemmanuelio@users.noreply.github.com>
2019-10-01 17:09:56 -0400

    Fiddlin maps

--- jscript/cpeum_video_carte.js
+++ jscript/cpeum_video_carte.js
@@ -6,17 +6,22 @@ $(document).ready(function () {
 // Création de la carte du "viewer"
 carte = L.map("cpeum-carte", {attributionControl: false, zoomControl: false, preferCanvas: true});
 carte.setView([45.508, -73.587], 8);
-L.tileLayer("http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-	attribution: "",//'&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-	maxZoom: 20,
+// L.tileLayer("http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+// 	attribution: "",//'&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+// 	maxZoom: 20,
+// }).addTo(carte);
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+	maxZoom: 19
 }).addTo(carte);
 
 // Création d'une carte pour les captures
 carteCapture = L.map("cpeum-carte-capture", {attributionControl: false, zoomControl: false, preferCanvas: true});
 carteCapture.setView([45.508, -73.587], 8);
-L.tileLayer("http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-	attribution: "",//'&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-	maxZoom: 20,
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
 }).addTo(carteCapture);
 
 videoTime = 0;
@@ -25,13 +30,13 @@ punaiseInited = false;
 // Suite à la lecture des fichiers importés (et suite à la conversion vers GeoJSON si fichier .gpx) : création du tracé gps et de la punaise
 cpeumGPS = function () {
 	if ( punaiseInited ) { carte.removeLayer(punaise); carteCapture.removeLayer(punaiseCapture) };
-	gpsLayer = L.geoJSON(gpsData, {color: "rgba(0,0,0,0.75)"}).addTo(carte);
-	gpsLayerCapture = L.geoJSON(gpsData, {color: "rgba(0,0,0,0.75)"}).addTo(carteCapture);
+	gpsLayer = L.geoJSON(gpsData, {color: "rgba(255,255,255,0.75)", weight: 4}).addTo(carte);
+	gpsLayerCapture = L.geoJSON(gpsData, {color: "rgba(0,0,0,0.75)", weight: 4}).addTo(carteCapture);
 	carte.flyToBounds(gpsLayer.getBounds(), {animate: true, duration: 1.5});
 	carteCapture.flyToBounds(gpsLayer.getBounds(), {animate: false});
 	punaiseLatLng = L.GeoJSON.coordsToLatLng(gpsData.geometry.coordinates[0]);
-	punaise = L.circleMarker(punaiseLatLng, {radius: 8, color: "rgb(250,252,255)", fillColor: "rgb(245,40,35)", fillOpacity: 1}).addTo(carte);
-	punaiseCapture = L.circleMarker(punaiseLatLng, {radius: 8, color: "rgb(250,252,255)", fillColor: "rgb(245,40,35)", fillOpacity: 1}).addTo(carteCapture);
+	punaise = L.circleMarker(punaiseLatLng, {radius: 8, color: "rgb(250,252,255)", fillColor: "rgb(35,45,245)", fillOpacity: 1}).addTo(carte);
+	punaiseCapture = L.circleMarker(punaiseLatLng, {radius: 8, color: "rgb(250,252,255)", fillColor: "rgb(35,45,245)", fillOpacity: 1}).addTo(carteCapture);
 	punaiseInited = true;
 }
 
